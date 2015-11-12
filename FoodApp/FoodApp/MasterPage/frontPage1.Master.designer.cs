fa9067469391ef1ab4cb1d480f03c09a9b57c94a
HieuDoan0412 <hieudoan412@gmail.com>
2015-11-12 16:09:10 +0200

    Revert "Fix frontPage1.Master.designer.cs bug and modify mobile version"
    This reverts commit f5f5f683a01361ad2c8062fcf25ce37534b34765.
    
    Conflicts:
    	FoodApp/FoodApp/MasterPage/frontPage1.Master.designer.cs

--- FoodApp/FoodApp/MasterPage/frontPage1.Master.designer.cs
+++ FoodApp/FoodApp/MasterPage/frontPage1.Master.designer.cs
@@ -7,6 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+<<<<<<< HEAD
 namespace FoodApp.MasterPage
 {
 
@@ -77,4 +78,72 @@ namespace FoodApp.MasterPage
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
     }
+=======
+
+
+public partial class  {
+    
+    /// <summary>
+    /// head control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+    
+    /// <summary>
+    /// form1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    
+    /// <summary>
+    /// accountInfo control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.ContentPlaceHolder accountInfo;
+    
+    /// <summary>
+    /// Recipes control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.ImageButton Recipes;
+    
+    /// <summary>
+    /// MyList control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.ImageButton MyList;
+    
+    /// <summary>
+    /// ContentPlaceHolder1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+    
+    /// <summary>
+    /// ContentPlaceHolder2 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+>>>>>>> parent of f5f5f68... Fix frontPage1.Master.designer.cs bug and modify mobile version
 }
