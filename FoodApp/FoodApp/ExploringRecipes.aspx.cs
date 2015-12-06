f4272a7320db07384ad7d065d34cbddfe92d932c
Kim Tran <trankimhieu1991@gmail.com>
2015-12-06 19:47:45 +0200

    explore recipe (without cookie for link)

--- FoodApp/FoodApp/ExploringRecipes.aspx.cs
+++ FoodApp/FoodApp/ExploringRecipes.aspx.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+//Import namespace OleDb for databases (outside class)
+using System.Data.OleDb;
+//System.Data for command object
+using System.Data;
 
 namespace FoodApp
 {
     public partial class ExploringRecipes : System.Web.UI.Page
     {
+        private OleDbConnection myConnection = new OleDbConnection();
+        private OleDbCommand cmd = new OleDbCommand();
+        String connstr = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = " + System.AppDomain.CurrentDomain.BaseDirectory + @"\Database\DatabaseforApp.mdb;";
+       
+
         protected void Page_Load(object sender, EventArgs e)
+
         {
-            
+            myConnection.ConnectionString = connstr;
+            myConnection.Open();
+            cmd.Connection = myConnection;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT Recipe.Name, Recipe.ImageURL, UserData.Username FROM UserData INNER JOIN Recipe ON UserData.UserDataID = Recipe.UserDataID; ";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd); 
+            da.Fill(dt);
+            RecipeList.DataSource = dt;
+            RecipeList.DataBind();
+
+            myConnection.Close();
         }
     }
 }
