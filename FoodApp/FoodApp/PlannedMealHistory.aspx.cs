37cad23d74de25c3c9097135d8265480244f2d68
Huy Trinh <trinhphandinhhuy@gmail.com>
2015-12-09 16:22:27 +0200

    Finish ShoppingListHistory + Planned Meal history

--- FoodApp/FoodApp/PlannedMealHistory.aspx.cs
+++ FoodApp/FoodApp/PlannedMealHistory.aspx.cs
@@ -12,11 +12,19 @@ using System.Web.UI.WebControls;
 
 namespace FoodApp
 {
-    public partial class ShoppingListHistory : System.Web.UI.Page
+    public partial class PlannedMealHistory : System.Web.UI.Page
     {
         private OleDbConnection myConnection = new OleDbConnection();
         string connstr = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = " + System.AppDomain.CurrentDomain.BaseDirectory + @"\Database\DatabaseforApp.mdb;";
         private int userID;
+
+        private void checkAuthentication()
+        {
+            if (Session["username"] == null || Session["username"].ToString() == "" || Session["userlevel"] == null || Session["userlevel"].ToString() == "")
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
 
         protected void Page_Init(object sender, EventArgs e)
         {
@@ -26,24 +34,23 @@ namespace FoodApp
             userID = Convert.ToInt32(Session["userid"].ToString());
         }
 
-        private void checkAuthentication()
+        private void getDB()
         {
-            if (Session["username"] == null || Session["username"].ToString() == "" || Session["userlevel"] == null || Session["userlevel"].ToString() == "")
-            {
-                Response.Redirect("Login.aspx");
-            }
-        }
-        protected void Page_Load(object sender, EventArgs e)
-        {
-             
-            OleDbCommand command = new OleDbCommand("SELECT * FROM ShoppingList WHERE UserDataID = " + userID, myConnection);
+            OleDbCommand command = new OleDbCommand("SELECT * FROM PlannedMeal WHERE UserDataID = " + userID, myConnection);
             command.ExecuteNonQuery();
             DataTable dt = new DataTable();
             OleDbDataAdapter da = new OleDbDataAdapter(command);
             da.Fill(dt);
-            ShoppingList.DataSource = dt;
-            ShoppingList.DataBind();
+            PlannedMeal.DataSource = dt;
+            PlannedMeal.DataBind();
             myConnection.Close();
         }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getDB();
+        }
+
+        
     }
 }
